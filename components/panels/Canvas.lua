5a5319fee684815919e619e5cc18aeed102f9ab7
NegativeName <negativenamengt@gmail.com>
2023-06-11 17:40:47 +0200

    Delete components directory

--- components/panels/Canvas.lua
+++
@@ -1,9 +0,0 @@
-WGui.NCanvas = WGui.Inherit(WGui.PanelWidget, "NCanvas", {
-    ChildSlot = "CanvasPanelSlot"
-})
-
-function WGui.NCanvas:Constructor()
-    self.Super:Constructor( "nwgui-assets::AW_Canvas" )
-    self:AddToViewport()
-    return self
-end
--- components/panels/Overlay.lua
+++
@@ -1,8 +0,0 @@
-WGui.NOverlay = WGui.Inherit(WGui.PanelWidget, "NOverlay", {
-    ChildSlot = "OverlaySlot"
-})
-
-function WGui.NOverlay:Constructor()
-    self.Super:Constructor( "nwgui-assets::AW_Overlay" )
-    return self
-end
